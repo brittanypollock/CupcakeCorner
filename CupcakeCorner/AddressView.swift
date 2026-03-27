e04fc490ac392e7c4305f91316629e02d9e28beb
Brittany Pollock <brittpollock15@gmail.com>
2026-03-26 20:50:59 -0700

    Created basic fuctionality & views for Cupcake Corner.

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,36 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Brittany Pollock on 3/26/26.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @Bindable var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip Code", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check Out") {
+                    CheckoutView(order: order)
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery Details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
